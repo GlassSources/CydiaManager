c385356d48bfbb9b6d331835ac60be3a126fe906
GlassSources <luaswat@lukkit.gq>
2015-07-01 16:12:33 -0400

    main code commit
    first code commit and concept/build

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cydia Manager
+This is my second attempt on making a more functional
+prototype for a pastebin version management system for updating and notifying
+users.
+
+## Compatiblity
+Currently only,
+      - CydiaScriptLoader
+
+is compatible with this plugin.
+
+### Future
+Hopefully, developers will send a request on adding their plugins to
+this program, so people can be notified easily on what plugins need to be updated
+for quick patches and security patches to get to the users.
+(basically allows your users to be notified on any update to plugins and possibly increase
+update rate.)
+
+# Not released?
+Yes, this has not been released as a full build and on the plugin repository.
+However, this plugin MAY get to the plugin repository if other people
+wants their plugin in this system. Not all systems work properly, because I was
+aiming for a quick concept and first build. 
---
+++ main.lua
@@ -0,0 +1,87 @@
+PLUGIN = nil
+internationTable = nil
+function findVersion(pastebinid)
+  local result = nil
+  local newWeb = "http://pastebin.com/raw.php?i=" .. pastebinid .. ""
+  local ConnectCallbacks =
+  {
+	OnConnected = function (a_Link)
+		-- Connection succeeded, send the http request:
+		a_Link:Send("GET " .. newWeb .. " HTTP/1.0\r\n")
+	end,
+
+	OnError = function (a_Link, a_ErrorCode, a_ErrorMsg)
+		-- Log the error to console:
+		LOG("An error has occurred while talking to " .. newWeb .. ": " .. a_ErrorCode .. " (" .. a_ErrorMsg .. ")")
+	end,
+
+	OnReceivedData = function (a_Link, a_Data)
+		Player:SendMessage("[CYDIA] Script recieved!")
+		result = a_Data
+	end,
+  }
+	cNetwork:Connect(newWeb .. "", 80, ConnectCallbacks)
+  return result
+end
+
+function Initialize(Plugin)
+  Plugin:SetName("CydiaManager")
+  Plugin:SetVersion(1)
+  cPluginManager:BindCommand("/manage", "cmanage.main", manageCommand, " ~ The main command for Cydia Management.")
+  local compatiblePlugins = {"CydiaScriptLoader"}
+  internationTable = compatiblePlugins
+  if cPluginManager:GetPlugin(compatiblePlugins[1]):GetStatus() == "psLoaded" then
+    LOG("CydiaManager - has found CydiaScriptLoader! Comparing..")
+    local localPastebin = cPluginManager:CallPlugin(compatiblePlugins[1], cydiaManagement())
+    local plugin = cPluginManager:GetPlugin(compatiblePlugins[1])
+    local localVersion = plugin:GetVersion()
+    if findVersion(localPastebin) == localVersion then
+      LOG("CydiaScriptLoader is at the latest version.")
+    end
+    if findVersion(localPastebin) > localVersion then
+      LOG("CydiaScriptLoader is outdated, please update.")
+    end
+    if findVersion(localPastebin) < localVersion then
+      LOG("CydiaScriptLoader is out of sync. Please recorrect this issue.")
+    end
+  else
+    LOG("CydiaManager - cannot find CydiaScriptLoader.")
+  end
+  PLUGIN = Plugin
+  return true
+end
+
+function manageCommand(Split, Player)
+  if (#Split ~= 2) then
+    Player:SendMessage("Usage: /manage [command]")
+    return true
+  end
+  if Split[2] == "refresh" then
+    cPluginManager:RefreshPluginList()
+  end
+  if Split[2] == "version" then
+    Player:SendMessage("CydiaManagement: is running build " .. PLUGIN:GetVersion())
+  end
+  if Split[2] == "recheck" then
+    if cPluginManager:GetPlugin(internationTable[1]):GetStatus() == "psLoaded" then
+      LOG("CydiaManager - has found CydiaScriptLoader! Comparing..")
+      local localPastebin = cPluginManager:CallPlugin(internationTable[1], cydiaManagement())
+      local plugin = cPluginManager:GetPlugin(internationTable[1])
+      local localVersion = plugin:GetVersion()
+      if findVersion(localPastebin) == localVersion then
+        LOG("CydiaScriptLoader is at the latest version.")
+      end
+      if findVersion(localPastebin) > localVersion then
+        LOG("CydiaScriptLoader is outdated, please update.")
+      end
+      if findVersion(localPastebin) < localVersion then
+        LOG("CydiaScriptLoader is out of sync. Please recorrect this issue.")
+      end
+    else
+      LOG("CydiaManager - cannot find CydiaScriptLoader.")
+    end
+  end
+  if Split[2] == "help" then
+    Player:SendMessage("Cydia Manager - help is not implemented in this build.")
+  end
+end
